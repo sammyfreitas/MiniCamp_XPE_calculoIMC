e192033971d92d4e9841574cf2f8245d8756df08
Tony Freitas <84025181+sammyfreitas@users.noreply.github.com>
2022-08-03 20:40:50 -0300

    Add files via upload
    Adicionadas faixas de peso para o IMC

--- js/main.js
+++ js/main.js
@@ -16,18 +16,39 @@ window.addEventListener('load', function() {
 
         var peso = parseInt(document.querySelector("#peso").value),
             altura = parseInt(document.querySelector("#altura").value);
+        var faixa = 'Peso Normal';
 
         // Calcular o IMC se os campos estiverem preenchidos com números with numbers
 
         if (!isNaN(peso) && !isNaN(altura)) {
 
             const IMC = ((peso / Math.pow(altura, 2)) * 10000).toFixed(2);
+
             console.log(peso);
+
+            if (IMC >= 16 & IMC < 16.99) {
+                faixa = ('Muito abaixo do peso');
+            } else if (IMC >= 17 & IMC < 18.49) {
+                faixa = ('Abaixo do peso');
+            } else if (IMC >= 18.5 & IMC < 24.99) {
+                faixa = ('Peso normal');
+            } else if (IMC >= 25 & IMC < 29.99) {
+                faixa = ('Acima do Peso');
+            } else if (IMC >= 30 & IMC < 34.99) {
+                faixa = ('Obesidade Grau I');
+            } else if (IMC >= 35 & IMC < 40) {
+                faixa = ('Obesidade Grau II');
+            } else if (IMC > 40) {
+                faixa = ('Obesidade Grau III');
+            } else {
+                faixa = ('Faixa Inválida');
+            }
+
 
             // Mostrar Resultado
 
             result.innerHTML = `
-            <p>Seu IMC é: ${IMC}</p>
+            <p><strong>Seu IMC é: ${IMC} e sua faixa é: ${faixa}</strong></p>
             `
         } else {
             result.innerHTML = `
